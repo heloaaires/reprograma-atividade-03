961dda6f6d2f5f611227d58b18212a67ba8f862e
heloaaires <heloa.andrade@gmail.com>
2022-04-15 17:14:39 -0300

    exercícios-2-e-3

---
+++ dia-da-semana.js
@@ -0,0 +1,25 @@
+//Elabore um algoritmo que receba um número (1-7) 
+//e devolva o dia da semana correspondente.
+
+function diaDaSemana(numeroDia) {
+    switch(numeroDia){
+        case '1':
+            return 'Domingo'
+        case '2':
+            return 'Segunda-feira'
+        case '3':
+            return 'Terça-feira'
+        case '4':
+            return 'Quarta-feira'
+        case '5':
+            return 'Quinta-feira'
+        case '6':
+            return 'Sexta-feira'
+        case '7':
+            return 'Sábado'
+        default:
+            return 'Não existe dia correspondente para esse número. Insira um valor de 1 a 7.'       
+
+    }
+}
+console.log(diaDaSemana("9"))
